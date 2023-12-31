88dd55c441d0f19ed9ef832583b5c9c353dee7e9
kesavakantipudi <147487877+kesavakantipudi@users.noreply.github.com>
2023-12-31 10:42:29 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(i%2==0)         {             for(j=1;j<=n;j++)             {                 printf("0 ");             }             printf(" ");         }         else         {             for(j=1;j<=n;j++)             {                 printf("1 ");             }             printf(" ");         }     } }

---
+++ Basic_Pattern_4.2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            for(j=1;j<=n;j++)
+            {
+                printf("0 ");
+            }
+            printf("
+");
+        }
+        else
+        {
+            for(j=1;j<=n;j++)
+            {
+                printf("1 ");
+            }
+            printf("
+");
+        }
+    }
+}
