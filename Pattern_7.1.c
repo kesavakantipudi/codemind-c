bc3a7fb8827673d99caf15e3bbf2b6c99564ff25
Kesava Sai Veerendra Kantipudi <147487877+kesavakantipudi@users.noreply.github.com>
2024-01-02 19:15:58 +0530

    #include<stdio.h> int main() {     int n,i,j,k,l;     scanf("%d",&n);     l=n;     for(i=1;i<=n;i++)     {         for(j=1;j<i;j++)         {             printf(" ");         }         for(k=1;k<=l;k++)         {             printf("* ");         }         l-=1;         printf(" ");     } }

---
+++ Pattern_7.1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k,l;
+    scanf("%d",&n);
+    l=n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<i;j++)
+        {
+            printf(" ");
+        }
+        for(k=1;k<=l;k++)
+        {
+            printf("* ");
+        }
+        l-=1;
+        printf("
+");
+    }
+}
