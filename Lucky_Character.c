1d1a404a9b85ea53d7e4603fc8913ed92beca215
Kesava Sai Veerendra Kantipudi <147487877+kesavakantipudi@users.noreply.github.com>
2024-02-22 10:56:03 +0530

    #include<stdio.h> int main() {     char str[10];     char temp;     scanf("%[^ ]s",str);     for(int i=0;str[i]!=NULL;i++)     {         temp=str[6];     }     printf("%c",temp); }

---
+++ Lucky_Character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char str[10];
+    char temp;
+    scanf("%[^
+]s",str);
+    for(int i=0;str[i]!=NULL;i++)
+    {
+        temp=str[6];
+    }
+    printf("%c",temp);
+}
